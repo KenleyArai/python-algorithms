418f17f7124d6807ecc4bc7ebc8ca04cb03a292a
Kenley Arai <me@kenleyarai.com>
2017-11-15 16:35:57 -0800

    Passed comparison tests

--- src/node.py
+++ src/node.py
@@ -2,3 +2,9 @@ class Node:
     def __init__(self, data=None, nextNode=None):
         self.data = data
         self.nextNode = nextNode
+
+    def __eq__(self, other):
+        return self._eq(self, other)
+
+    def _eq(self, other_node):
+        return self.data == other_node.data
--- test/test_node.py
+++ test/test_node.py
@@ -10,4 +10,17 @@ class TestNode(object):
     def test_assign_next(self):
         test_node = Node(123)
         test_node2 = Node(456, test_node)
+
         assert test_node2.nextNode.data == 123
+
+    def test_eq_same(self):
+        test_node = Node(123)
+        test_node2 = Node(123)
+
+        assert test_node._eq(test_node2) == True
+
+    def test_eq_diff(self):
+        test_node = Node(123)
+        test_node2 = Node(456)
+
+        assert test_node._eq(test_node2) == False
