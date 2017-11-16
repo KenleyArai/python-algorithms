3b6bd5216dcda3716790e6b9c79fee0427b6a58e
Kenley Arai <me@kenleyarai.com>
2017-11-16 11:32:48 -0800

    Hashtable and hashtable tests setup

--- HashTable/src/hashtable.py
+++ HashTable/src/hashtable.py
@@ -1,2 +1,27 @@
+from src.hash import Hash
+
+
 class Hashtable:
-    pass
+
+    def __init__(self, size=256):
+        self._size = size
+        self._indexes = [None] * size
+        self._data = [None] * size
+
+    def __delitem__(self, obj):
+        return
+
+    def __len__(self):
+        return 0
+
+    def __setitem__(self, obj, val):
+        return 0
+
+    def __getitem__(self, obj, objtype):
+        return 0
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        raise StopIteration
--- HashTable/test/test_hashtable.py
+++ HashTable/test/test_hashtable.py
@@ -2,17 +2,36 @@ from src.hashtable import Hashtable
 
 
 class TestHashTable:
-    def test_in(self):
-        assert False
+    def test_put(self):
+        ht = Hashtable(15)
+        ht[0] = "Panda Bear"
 
-    def test_len(self):
-        assert False
+        assert ht._data[0] == "Panda Bear"
 
     def test_get(self):
-        assert False
+        ht = Hashtable(15)
+        ht[12] = "Panda Bear"
 
-    def test_put(self):
-        assert False
+        assert ht[12] == "Panda Bear"
+
+    def test_len(self):
+        ht = Hashtable(15)
+        ht[7] = "Holder"
+        ht[2] = "Candy Cane"
+        ht[12] = "Panda Bear"
+
+        assert len(ht) == 3
 
     def test_del(self):
-        assert False
+        ht = Hashtable(15)
+        ht[0] = "Panda Bear"
+        del ht[0]
+        assert ht._data[0] != "Panda Bear"
+
+    def test_in(self):
+        ht = Hashtable(15)
+        ht[7] = "Holder"
+        ht[2] = "Candy Cane"
+        ht[12] = "Panda Bear"
+
+        assert "Panda Bear" in ht
