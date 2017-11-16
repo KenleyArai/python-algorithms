a1ed26b7376326f108d6d12e81126e492176de08
Kenley Arai <me@kenleyarai.com>
2017-11-16 11:07:45 -0800

    Setup other tests

--- HashTable/test/test_hashtable.py
+++ HashTable/test/test_hashtable.py
@@ -2,5 +2,17 @@ from src.hashtable import Hashtable
 
 
 class TestHashTable:
-    def test_in:
-        False
+    def test_in(self):
+        assert False
+
+    def test_len(self):
+        assert False
+
+    def test_get(self):
+        assert False
+
+    def test_put(self):
+        assert False
+
+    def test_del(self):
+        assert False
