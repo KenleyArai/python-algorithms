abf3bb09211d86e08268dded8eeb35ff706eb202
Kenley Arai <me@kenleyarai.com>
2017-11-17 15:53:42 -0800

    Setup heap nodes

---
+++ Heap/test/test_heap.py
@@ -0,0 +1,5 @@
+from src.heap import Heap
+
+
+class TestHeap:
+    pass
---
+++ Heap/test/test_node.py
@@ -0,0 +1,23 @@
+from src.node import Node
+
+
+class TestNode:
+
+    def test_creation(self):
+        n = Node(123)
+        assert n._data == 123 and n._left == None and n._right == None
+
+    def test_gt(self):
+        n1 = Node(10)
+        n2 = Node(5)
+        assert n1 > n2
+
+    def test_lt(self):
+        n1 = Node(5)
+        n2 = Node(10)
+        assert n1 < n2
+
+    def test_eq(self):
+        n1 = Node(10)
+        n2 = Node(10)
+        assert n1 == n2
