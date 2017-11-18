5f5b10704b6fa41dc32e20621bea4905e695bd6d
Kenley Arai <me@kenleyarai.com>
2017-11-17 16:08:33 -0800

    heap is setup

--- Heap/src/heap.py
+++ Heap/src/heap.py
@@ -6,3 +6,27 @@ class Heap:
     def __init__(self, heaptype='min'):
         self._head = Node()
         self._type = heaptype
+
+     def push(self):
+        pass
+
+    def pop(self, data):
+        pass
+
+    def empty(self):
+        pass
+
+    def peek(self):
+        pass
+
+    def size(self):
+        pass
+
+    def heapify(self, data):
+        pass
+
+    def sift_up(self):
+        pass
+
+    def sift_down(self):
+        pass
