a6baac4af00d0fef90b3d7f45d8c723b20c9f56b
Kenley Arai <me@kenleyarai.com>
2017-11-18 19:06:21 -0800

    Setup trivial functions for graph

--- Graph/src/graph.py
+++ Graph/src/graph.py
@@ -11,41 +11,40 @@ class Graph:
         return self._type == 'list'
 
     def are_adjacent(self, x, y):
-        # tests whether there is an edge from the vertex x to the vertex y;
-        pass
+        return self._graph.are_adjacent(x, y)
 
     def get_neighbors(self, x):
         # lists all vertices x such that there is an edge from the vertex x to the vertex y;
-        pass
+        return self._graph.get_neighbors(x)
 
     def add_vertex(self, x):
         # adds the vertex x
-        pass
+        return self._graph.add_vertex(x)
 
     def remove_vertex(self, x):
         # removes the vertex x, if it is there;
-        pass
+        return self._graph.remove_vertex(x)
 
     def add_edge(self, x, y):
         # adds the edge from the vertex x to the vertex y, if it is not there;
-        pass
+        return self._graph.add_edge(x, y)
 
     def remove_edge(self, x, y):
         # removes the edge from the vertex x to the vertex y, if it is there;
-        pass
+        return self._graph.remove_edge(x, y)
 
     def get_vertex_value(self, x):
         # returns the value associated with the vertex x;
-        pass
+        return self._graph.get_vertex_value(x)
 
     def set_vertex_value(self, x):
         # sets the value associated with the vertex x to v.
-        pass
+        return self._graph.set_edge_value(x)
 
     def get_edge_value(self, x, y):
         # returns the value associated with the edge (x, y)
-        pass
+        return self._graph.get_edge_value(x, y)
 
     def set_edge_value(self, x, y):
         # sets the value associated with the edge (x, y) to v.
-        pass
+        return self._graph.set_edge_value(x, y)
